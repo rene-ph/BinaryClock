c7a666d08d45dcd0ac62a3afea1b1d78e5a9d279
Rene H <rphs09@hotmail.com>
2019-07-06 14:38:37 -0500

    Added refactor to use Util file

--- src/Components/Clock/Seconds/Seconds.js
+++ src/Components/Clock/Seconds/Seconds.js
@@ -1,58 +1,16 @@
 import React, {Component} from 'react';
+
 import './Seconds.css';
 import Dot from '../Dot/Dot';
 import EmptyDot from '../EmptyDot/EmptyDot';
+import Util from '../../../Utils/Util';
 
 class Seconds extends Component  {
  
-  static binaryNumbers = {}
+  static binaryNumbers = {};
 
-  constructor(props) {
-    super(props);
-    this.binaryNumbers = {
-      firstCol: 0,
-      secondCol: 0
-    }
-  }
-
-  convertToBinary(dec) {
-    var bits = [];
-    var dividend = dec;
-    var remainder = 0;
-    while (dividend >= 2) {
-        remainder = dividend % 2;
-        bits.push(remainder);
-        dividend = (dividend - remainder) / 2;
-    }
-    bits.push(dividend);
-    return bits.join("");
-}
-
-  transformDigits = (digit) => {
-    let digits = [...digit.toString()];
-    let firstDigit; 
-    let secondDigit;
-    let binaryFirstColumn;
-    let binarySecondColumn;
-    if(digits[1] === undefined && digits[0] !== "0") {
-      firstDigit = 0;
-      secondDigit = digits[0];
-    }else if(digits[0] === "0" ) {
-      firstDigit = "6";
-      secondDigit = "0";
-    }
-    else {
-      firstDigit = digits[0];
-      secondDigit = digits[1];
-    }
-    binaryFirstColumn = this.convertToBinary(parseInt(firstDigit));
-    binarySecondColumn = this.convertToBinary(parseInt(secondDigit));
-    this.binaryNumbers = { firstCol: [...binaryFirstColumn], secondCol: [...binarySecondColumn]};
-  }
-
- 
   render() {
-    this.transformDigits(this.props.digit);
+   this.binaryNumbers = Util.convertToBinaryColumns(this.props.digit);
     return (
         <div className="Seconds">
             <div className="Column">
